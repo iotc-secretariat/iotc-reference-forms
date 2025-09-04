7c4c83174f41c4e8356ba1ffa7d0270363005911
Emmanuel Chassot <EChassot@iotc.local>
2025-09-04 16:37:26 +0400

    Repacement of Weight Estimation Method by Weight Measurement Tool

--- 01.initialise.R
+++ 01.initialise.R
@@ -268,7 +268,9 @@ TAG_TYPES = read_codelist(domain = "biology", schema = "biology", codelist = "TA
 
 DEPREDATION_SOURCES = read_codelist(domain = "biology", schema = "biology", codelist = "DEPREDATION_SOURCES")
 
-WEIGHT_ESTIMATION_METHODS = MEASUREMENT_TOOLS[TYPE_OF_MEASUREMENT_CODE  == "WG", -c("TYPE_OF_MEASUREMENT_CODE")]
+#WEIGHT_ESTIMATION_METHODS = MEASUREMENT_TOOLS[TYPE_OF_MEASUREMENT_CODE  == "WG", -c("TYPE_OF_MEASUREMENT_CODE")]
+
+WEIGHT_MEASUREMENT_TOOLS = read_codelist(domain = "biology", schema = "biology", codelist = "WEIGHT_MEASUREMENT_TOOLS")
 
 BAIT_TYPES = read_codelist(domain = "biology", schema = "biology", codelist = "BAIT_TYPES")
 
